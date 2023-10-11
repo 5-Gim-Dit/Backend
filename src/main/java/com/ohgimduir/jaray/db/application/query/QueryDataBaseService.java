1c271bffba6032ab82e0032605bcb73c48372e0e
최수원 <suwon3274@gmail.com>
2023-10-11 22:43:42 +0900

    ......

--- src/main/java/com/ohgimduir/jaray/db/application/query/QueryDataBaseService.java
+++ src/main/java/com/ohgimduir/jaray/db/application/query/QueryDataBaseService.java
@@ -1,7 +1,24 @@
 package com.ohgimduir.jaray.db.application.query;
 
+import com.ohgimduir.jaray.db.domain.entity.DataBase;
+import com.ohgimduir.jaray.db.domain.repository.DataBaseRepository;
+import com.ohgimduir.jaray.security.oauth.helper.SecurityHelper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class QueryDataBaseService {
+
+    private final DataBaseRepository dataBaseRepository;
+    private final SecurityHelper securityHelper;
+
+    public List<DataBase> getMy() {
+        return dataBaseRepository.findByMemberId(securityHelper.getMemberId());
+    }
+
 }
