21fca22a6b80299191e3e06b11253913e9c0f9f6
최수원 <suwon3274@gmail.com>
2023-10-11 14:33:49 +0900

    init

---
+++ src/main/java/com/ohgimduir/jaray/JarayApplication.java
@@ -0,0 +1,13 @@
+package com.ohgimduir.jaray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarayApplication.class, args);
+    }
+
+}
