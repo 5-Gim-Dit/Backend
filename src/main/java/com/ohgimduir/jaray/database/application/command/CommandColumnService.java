c3269c91bc948ef302dd70a95756b8251303d863
최수원 <suwon3274@gmail.com>
2023-10-12 10:21:35 +0900

    ..................

--- src/main/java/com/ohgimduir/jaray/database/application/command/CommandColumnService.java
+++ src/main/java/com/ohgimduir/jaray/database/application/command/CommandColumnService.java
@@ -1,9 +1,23 @@
 package com.ohgimduir.jaray.database.application.command;
 
+import com.ohgimduir.jaray.common.annotations.CommandService;
+import com.ohgimduir.jaray.database.application.command.request.CreateColumnRequest;
+import com.ohgimduir.jaray.database.domain.entity.Column;
+import com.ohgimduir.jaray.database.domain.repository.ColumnRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
+@CommandService
 @RequiredArgsConstructor
 public class CommandColumnService {
+
+    private final ColumnRepository columnRepository;
+
+    public void create(long tableId, CreateColumnRequest request) {
+        columnRepository.save(Column.ExceptIdBuilder()
+                .name(request.name())
+                .type(request.type())
+                .tableId(tableId)
+                .build()).getId();
+    }
+
 }
