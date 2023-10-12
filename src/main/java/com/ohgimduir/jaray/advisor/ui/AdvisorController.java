4e950033d916e3d0df497536555815d93094886c
Woongbin06 <kob05314@gmail.com>
2023-10-12 22:02:00 +0900

    FEAT :: .

--- src/main/java/com/ohgimduir/jaray/advisor/ui/AdvisorController.java
+++ src/main/java/com/ohgimduir/jaray/advisor/ui/AdvisorController.java
@@ -4,6 +4,8 @@ import com.ohgimduir.jaray.advisor.application.AdvisorService;
 import com.ohgimduir.jaray.advisor.application.request.ChatGPTQuery;
 import com.ohgimduir.jaray.advisor.application.response.ChatGptResponse;
 import com.ohgimduir.jaray.advisor.application.response.QuestionRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "챗 지피 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/gpt")
@@ -21,6 +24,7 @@ public class AdvisorController {
     private final AdvisorService advisorService;
 
     @PostMapping("/query")
+    @Operation(description = "챗 지피티에게 질문하기")
     public String callQueryApi(@RequestBody QuestionRequest request) {
 
         ChatGptResponse response = null;
