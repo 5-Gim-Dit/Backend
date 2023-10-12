f5f04264ea088357a779f64d9d822aa4d412cc0b
최수원 <suwon3274@gmail.com>
2023-10-12 19:44:50 +0900

    .......................

---
+++ src/main/java/com/ohgimduir/jaray/advisor/application/AdvisorService.java
@@ -0,0 +1,9 @@
+package com.ohgimduir.jaray.advisor.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdvisorService {
+}
---
+++ src/main/java/com/ohgimduir/jaray/advisor/ui/AdvisorController.java
@@ -0,0 +1,4 @@
+package com.ohgimduir.jaray.advisor.ui;
+
+public class AdvisorController {
+}
